dcc5bf2ca7b5c39b7832f8e1c1674a74f643eb68
NicolasRanniery <nicolasranniery123@gmail.com>
2025-10-22 11:52:07 -0300

    Mudança nas cores e fontes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,16 @@ import Universidades from "@/components/Universidades";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center text-center px-4 flex-1 bg-[#e9ded2]">
-      <h1 className="text-5xl font-bold mb-4 text-gray-900">
-        Bem vindo ao Remanescentes
+      <p className="font-texgyretermes text-2xl md:text-2xl text-[#303030]">Seja bem vindo ao</p>
+      <h1 className="text-6xl md:text-8xl mb-4 text-[#303030] font-texgyretermes underline decoration-2 underline-offset-8 -mt-3 md:-mt-5">
+        Remanescentes
       </h1>
-      <p className="max-w-xl text-2xl">
+      <p className="max-w-xl text-xl md:text-3xl font-texgyretermes italic text-[#303030]">
         Fazendo Deus ser conhecido nas universidades
       </p>
-      <section className="py-16 px-8 text-center">
+      <section className="py-10 md:py-16 px-4 md:px-8 text-center">
         {/* Container dos professores */}
-        <div className="flex flex-wrap justify-center gap-20">
+        <div className="flex flex-wrap justify-center gap-10 md:gap-20">
           {/* <Universidades caminho="remanescentes-ufpa" nome="UFPA" /> */}
           <Universidades caminho="remanescentes-ufpa" nome="UFPA" />
           <Universidades caminho="remanescentes-ufra" nome="UFRA" />
