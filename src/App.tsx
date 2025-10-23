604b441ca35dd48f3ce64186a110ea814311c5f8
Nicolas Ranniery <nicolas.sousa@pge.pa.gov.br>
2025-10-23 19:22:04 -0300

    Criação da aba para 'Erro 404'

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import Contato from "./pages/Contato";
 import FacaParte from "./pages/FacaParte";
 import QuemSomos from "./pages/QuemSomos";
 import Footer from "./components/Footer";
+import Erro404 from "./pages/Erro404";
 
 function App() {
   return (
     <div className="min-h-screen text-gray-800 flex flex-col overflow-auto">
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/quem-somos" element={<QuemSomos />} />
-          <Route path="/faca-parte" element={<FacaParte />} />
-          <Route path="/contato" element={<Contato />} />
-        </Routes>
-        <Footer />
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quem-somos" element={<QuemSomos />} />
+        <Route path="/faca-parte" element={<FacaParte />} />
+        <Route
+          path="*"
+          element={<Erro404 />}
+        />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
